e3927248804007a37882add4f22085d67e366cc7
Grigory Vasilev <grishavasilev62@gmail.com>
2022-11-26 14:30:10 +0300

    resolve 1 point

--- Solution/Task 1/task1Solution.py
+++ Solution/Task 1/task1Solution.py
@@ -2,13 +2,10 @@ import dataFrameLoader as dfLoader
 
 
 # Show dataframe as table.
-def print_dataframe(data_frame_url):
-    data_frame = dfLoader.load_data_frame(data_frame_url)
+data_frame_url = 'https://raw.githubusercontent.com/Grossmend/CSV/master/titanic/data.csv'
+data_frame = dfLoader.load_data_frame(data_frame_url)
+print(data_frame)
 
-    print(data_frame)
-
-
-print_dataframe('https://raw.githubusercontent.com/Grossmend/CSV/master/titanic/data.csv')
 
 
 
